5d027ef3890427157561c112c465a155332e7d3c
Cao Nguyen Van <cao.nv17@gmail.com>
2019-11-29 15:05:32 +0700

    update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,14 @@
-// module.exports = {
-//   presets: ['module:metro-react-native-babel-preset'],
-//   plugins: [
-//     require.resolve('babel-plugin-module-resolver'),
-//     {
-//       root: ["./"],
-//       alias: {
-//         "@components": "./src/components"
-//       }
-//     }
-//   ]
-// };
-
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['module-resolver', {
-      'root': ['./'],
-      'alias': {
-        '@': './src/',
-        '@base/ui-kit': './packages/base-ui-kit',
-        '@base/post': './packages/base-post',
-        '@components': './src/components/',
-        '@store': './src/store/',
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ["./"],
+      alias: {
+        "@components": "./src/components"
       }
-    }]
-  ],
+    }
+  ]
 };
+
+
